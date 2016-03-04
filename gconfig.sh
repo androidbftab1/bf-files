fcb0751d22803135ec0c8c61cf9bf27bf40f0896
android.bf.tab1 <android.bf.tab1@gmail.com>
2016-03-04 08:30:51 +0800

    update gconfig.sh

--- gconfig.sh
+++ gconfig.sh
@@ -1,2 +1,4 @@
 git config --global user.name "android.bf.tab1"
 git config --global user.email "android.bf.tab1@gmail.com"
+git config --global push.default matching
+git config --global push.default simple
